b8d18dea29e8d42f45a33ead499350a7e967cfb2
Brad Larsen <bradford.larsen@praetorian.com>
2023-01-17 12:08:09 -0500

    Add more CLI tests; refine existing ones

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,7 +2,9 @@ use assert_cmd::prelude::*;
 use assert_fs::prelude::*;
 use assert_fs::{fixture::ChildPath, TempDir};
 use insta::{assert_display_snapshot, assert_snapshot, with_settings};
+use lazy_static::lazy_static;
 // use predicates::prelude::*;
+use predicates::str::RegexPredicate;
 use std::path::Path;
 use std::process::Command;
 
@@ -11,6 +13,8 @@ use std::process::Command;
 // -------------------------------------------------------------------------------------------------
 
 /// Use `insta` to do snapshot testing against a command's exit code, stdout, and stderr.
+///
+/// The given expression should be an `assert_cmd::assert::Assert`.
 macro_rules! assert_cmd_snapshot {
     ( $cmd:expr ) => {
         let cmd = $cmd;
@@ -25,6 +29,7 @@ macro_rules! assert_cmd_snapshot {
 }
 
 /// Build a `Command` for the `noseyparker` crate binary with variadic command-line arguments.
+///
 /// The arguments can be anything that is allowed by `Command::arg`.
 macro_rules! noseyparker {
     ( $( $arg:expr ),* ) => {
@@ -38,21 +43,61 @@ macro_rules! noseyparker {
     }
 }
 
-/// Build a `Command` for the `noseyparker` crate binary.
-fn noseyparker() -> Command {
-    Command::cargo_bin("noseyparker").expect("noseyparker should be executable")
+lazy_static! {
+    static ref NOSEYPARKER: escargot::CargoRun = escargot::CargoBuild::new()
+        .bin("noseyparker")
+        .run()
+        .expect("noseyparker should be available");
+
+    // We could use this to write tests for specific feature configurations:
+    /*
+    static ref NOSEYPARKER_RULE_PROFILING: escargot::CargoRun = escargot::CargoBuild::new()
+        .bin("noseyparker")
+        .no_default_features()
+        .features("rule_profiling")
+        .run()
+        .expect("noseyparker with rule_profiling should be available");
+    */
 }
 
-fn is_match(pat: &str) -> predicates::str::RegexPredicate {
+/// Build a `Command` for the `noseyparker` crate binary.
+pub fn noseyparker() -> Command {
+    // Command::cargo_bin("noseyparker").expect("noseyparker should be executable")
+    NOSEYPARKER.command()
+}
+
+/// Create a `RegexPredicate` from the given pattern.
+pub fn is_match(pat: &str) -> RegexPredicate {
     predicates::str::is_match(pat).expect("pattern should compile")
 }
 
+/// Create a `RegexPredicate` for matching a scan stats output message from Nosey Parker.
+pub fn match_scan_stats(
+    num_bytes: &str,
+    num_blobs: u64,
+    new_matches: u64,
+    total_matches: u64,
+) -> RegexPredicate {
+    is_match(&format!(
+        r"^Scanned {} from {} blobs in .*; {}/{} new matches",
+        num_bytes, num_blobs, new_matches, total_matches
+    ))
+}
+
+/// Create a `RegexPredicate` for matching a "nothing was scanned" scan stats output message from
+/// Nosey Parker.
+pub fn match_nothing_scanned() -> RegexPredicate {
+    match_scan_stats("0B", 0, 0, 0)
+}
+
+/// A type to represent a mock scanning environment for testing Nosey Parker.
 pub struct ScanEnv {
     pub root: TempDir,
     pub datastore: ChildPath,
 }
 
 impl ScanEnv {
+    /// Create a new mock scanning environment.
     pub fn new() -> Self {
         let root = TempDir::new().expect("should be able to create tempdir");
         let datastore = root.child("datastore");
@@ -61,6 +106,7 @@ impl ScanEnv {
         Self { root, datastore }
     }
 
+    /// Create an empty file within this mock scanning environment with the given name.
     pub fn input_file(&self, name: &str) -> ChildPath {
         let input = self.root.child(name);
         input.touch().expect("should be able to write input file");
@@ -68,6 +114,7 @@ impl ScanEnv {
         input
     }
 
+    /// Create an empty directory within this mock scanning environment with the given name.
     pub fn input_dir(&self, name: &str) -> ChildPath {
         let input = self.root.child(name);
         input
@@ -77,10 +124,14 @@ impl ScanEnv {
         input
     }
 
+    /// Create a name for a child entry within this mock scanning environment.
+    ///
+    /// The filesystem is not touched by this function; this merely produces a `ChildPath`.
     pub fn child(&self, name: &str) -> ChildPath {
         self.root.child(name)
     }
 
+    /// Get the path to the Nosey Parker datastore directory within this mock scanning environment.
     pub fn dspath(&self) -> &Path {
         self.datastore.path()
     }
@@ -138,7 +189,7 @@ fn test_noseyparker_scan_emptydir() {
     noseyparker!("scan", "--datastore", scan_env.dspath(), input.path())
         .assert()
         .success()
-        .stdout(is_match(r#"^Scanned 0B from 0 blobs in .*; 0/0 new matches"#));
+        .stdout(match_nothing_scanned());
 }
 
 #[test]
@@ -148,7 +199,7 @@ fn test_noseyparker_scan_datastore_argorder() {
     noseyparker!("scan", input.path(), "--datastore", scan_env.dspath())
         .assert()
         .success()
-        .stdout(is_match(r#"^Scanned 0B from 0 blobs in .*; 0/0 new matches"#));
+        .stdout(match_nothing_scanned());
 }
 
 #[test]
@@ -158,7 +209,7 @@ fn test_noseyparker_scan_datastore_short() {
     noseyparker!("scan", "-d", scan_env.dspath(), input.path())
         .assert()
         .success()
-        .stdout(is_match(r#"^Scanned 0B from 0 blobs in .*; 0/0 new matches"#));
+        .stdout(match_nothing_scanned());
 }
 
 #[test]
@@ -169,7 +220,7 @@ fn test_noseyparker_scan_datastore_envvar() {
         .env("NP_DATASTORE", scan_env.dspath())
         .assert()
         .success()
-        .stdout(is_match(r#"^Scanned 0B from 0 blobs in .*; 0/0 new matches"#));
+        .stdout(match_nothing_scanned());
 }
 
 #[test]
@@ -179,7 +230,7 @@ fn test_noseyparker_scan_emptyfile() {
     noseyparker!("scan", "--datastore", scan_env.dspath(), input.path())
         .assert()
         .success()
-        .stdout(is_match(r#"^Scanned 0B from 1 blobs in .*; 0/0 new matches"#));
+        .stdout(match_scan_stats("0B", 1, 0, 0));
 }
 
 #[test]
@@ -190,5 +241,46 @@ fn test_noseyparker_scan_emptyfiles() {
     noseyparker!("scan", "--datastore", scan_env.dspath(), input1.path(), input2.path())
         .assert()
         .success()
-        .stdout(is_match(r#"^Scanned 0B from 2 blobs in .*; 0/0 new matches"#));
+        .stdout(match_scan_stats("0B", 2, 0, 0));
+}
+
+#[test]
+fn test_noseyparker_scan_file_symlink() {
+    let scan_env = ScanEnv::new();
+    let empty_file = scan_env.input_file("empty_file");
+    let input = scan_env.child("empty_file_link");
+    input
+        .symlink_to_file(empty_file)
+        .expect("should be able to create input symlink");
+    noseyparker!("scan", "--datastore", scan_env.dspath(), input.path())
+        .assert()
+        .success()
+        .stdout(match_nothing_scanned());
+}
+
+#[test]
+fn test_noseyparker_scan_file_maxsize() {
+    let scan_env = ScanEnv::new();
+    let input = scan_env.input_file("bigfile.dat");
+    input
+        .write_binary(&[b'a'; 1024 * 1024 * 10])
+        .expect("should be able to write input file contents");
+
+    // By default the input file gets scanned
+    noseyparker!("scan", "--datastore", scan_env.dspath(), input.path())
+        .assert()
+        .success()
+        .stdout(match_scan_stats("10.00 MiB", 1, 0, 0));
+
+    // With a restricted max file size, the file is not scanned
+    noseyparker!("scan", "--datastore", scan_env.dspath(), input.path(), "--max-file-size", "5")
+        .assert()
+        .success()
+        .stdout(match_nothing_scanned());
+
+    // Also check for the floating-point version of max file size
+    noseyparker!("scan", "--datastore", scan_env.dspath(), input.path(), "--max-file-size", "5.00")
+        .assert()
+        .success()
+        .stdout(match_nothing_scanned());
 }
