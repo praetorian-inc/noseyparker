1cdeb41351aebc5dae3bf4beb7d8e006bbb2a7fb
Brad Larsen <bradford.larsen@praetorian.com>
2023-11-01 17:39:46 -0400

    Include LICENSE and README.md in releases
    Fixes #89.

--- scripts/create-release.zsh
+++ scripts/create-release.zsh
@@ -109,6 +109,13 @@ banner "Assembling release dir"
 NP="$PWD/$RELEASE_DIR/bin/noseyparker"
 cp -p "$CARGO_BUILD_DIR/noseyparker-cli" "$NP"
 
+# Copy LICENSE and README.md
+cp -p LICENSE "$RELEASE_DIR/"
+cp -p README.md "$RELEASE_DIR/"
+
+################################################################################
+# Strip release binary if requested
+################################################################################
 if (( $DO_STRIP )); then
     banner "Stripping release binary"
     $STRIP_COMMAND "$NP"
