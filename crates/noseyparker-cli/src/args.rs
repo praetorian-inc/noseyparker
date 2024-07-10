praetorian-inc/noseyparker

commit c2af463e2e6c250ec52b0246e7f0e8e4d30c1f00

Account for RAM in default scan `-j` value (#174)
The default scan parallelism setting is now based on both number of CPUs and system RAM instead of just CPUs.

crates/noseyparker-cli/src/args.rs
@@ -5,12 +5,29 @@
 use clap::{
     crate_description, crate_version, ArgAction, Args, Parser, Subcommand, ValueEnum, ValueHint,
 };
+use lazy_static::lazy_static;
 use std::io::IsTerminal;
 use std::path::{Path, PathBuf};
 use strum::Display;
 use url::Url;
 
 use noseyparker::git_url::GitUrl;
+
+// -----------------------------------------------------------------------------
+// system information
+// -----------------------------------------------------------------------------
+lazy_static! {
+    /// How much RAM is installed in the system?
+    static ref RAM_GB: Option<f64> = {
+        if sysinfo::IS_SUPPORTED_SYSTEM {
+            use sysinfo::{System,RefreshKind,MemoryRefreshKind};
+            let s = System::new_with_specifics(RefreshKind::new().with_memory(MemoryRefreshKind::new().with_ram()));
+            Some(s.total_memory() as f64 / 1024.0 / 1024.0 / 1024.0)
+        } else {
+            None
+        }
+    };
+}
 
 // -----------------------------------------------------------------------------
 // utilities
@@ -76,10 +93,18 @@ pub fn validate_github_api_url(github_api_url: &Url, all_github_organizations: b
     }
 }
 
-fn get_parallelism() -> usize {
-    match std::thread::available_parallelism() {
-        Err(_e) => 1,
-        Ok(v) => v.into(),
+/// How many parallel scan jobs should be used by default?
+///
+/// This is based on the number of available vCPUs, and also takes into account the amount of
+/// memory per core.
+fn default_scan_jobs() -> usize {
+    match (std::thread::available_parallelism(), *RAM_GB) {
+        (Ok(v), Some(ram_gb)) => {
+            let n: usize = v.into();
+            n.clamp(1, (ram_gb / 4.0).floor() as usize)
+        }
+        (Ok(v), None) => v.into(),
+        (Err(_e), _) => 1,
     }
 }
 
@@ -285,7 +310,7 @@ pub struct AdvancedArgs {
     )]
     pub rlimit_nofile: u64,
 
-    /// Set the cache size for sqlite connections to SIZE
+    /// Set the cache size for SQLite connections to SIZE
     ///
     /// This has the effect of setting SQLite's `pragma cache_size=SIZE`.
     /// The default value is set to use a maximum of 1GiB for database cache.
@@ -554,7 +579,7 @@ pub struct ScanArgs {
     pub datastore: PathBuf,
 
     /// Use N parallel scanning threads
-    #[arg(long("jobs"), short('j'), value_name="N", default_value_t=get_parallelism())]
+    #[arg(long("jobs"), short('j'), value_name="N", default_value_t=default_scan_jobs())]
     pub num_jobs: usize,
 
     #[command(flatten)]

crates/noseyparker-cli/Cargo.toml
@@ -83,6 +83,7 @@ serde = { version = "1.0", features = ["derive"] }
 serde_json = "1.0"
 serde-sarif = "0.4"
 strum = { version = "0.26", features = ["derive"] }
+sysinfo = { version =  "0.30" }
 tar = "0.4"
 tempfile = "3.1"
 time = "0.3"

crates/noseyparker-cli/tests/help/snapshots/test_noseyparker__help__help-2.snap
@@ -68,7 +68,7 @@ Advanced Global Options:
           [default: 16384]
 
       --sqlite-cache-size <SIZE>
-          Set the cache size for sqlite connections to SIZE
+          Set the cache size for SQLite connections to SIZE
           
           This has the effect of setting SQLite's `pragma cache_size=SIZE`. The default value is set
           to use a maximum of 1GiB for database cache. See

crates/noseyparker-cli/tests/help/snapshots/test_noseyparker__help__help_datastore-2.snap
@@ -56,7 +56,7 @@ Advanced Global Options:
           [default: 16384]
 
       --sqlite-cache-size <SIZE>
-          Set the cache size for sqlite connections to SIZE
+          Set the cache size for SQLite connections to SIZE
           
           This has the effect of setting SQLite's `pragma cache_size=SIZE`. The default value is set
           to use a maximum of 1GiB for database cache. See

crates/noseyparker-cli/tests/help/snapshots/test_noseyparker__help__help_github-2.snap
@@ -68,7 +68,7 @@ Advanced Global Options:
           [default: 16384]
 
       --sqlite-cache-size <SIZE>
-          Set the cache size for sqlite connections to SIZE
+          Set the cache size for SQLite connections to SIZE
           
           This has the effect of setting SQLite's `pragma cache_size=SIZE`. The default value is set
           to use a maximum of 1GiB for database cache. See
@@ -83,4 +83,3 @@ Advanced Global Options:
           
           [default: true]
           [possible values: true, false]
-

crates/noseyparker-cli/tests/help/snapshots/test_noseyparker__help__help_github_repos-2.snap
@@ -64,7 +64,7 @@ Advanced Global Options:
           [default: 16384]
 
       --sqlite-cache-size <SIZE>
-          Set the cache size for sqlite connections to SIZE
+          Set the cache size for SQLite connections to SIZE
           
           This has the effect of setting SQLite's `pragma cache_size=SIZE`. The default value is set
           to use a maximum of 1GiB for database cache. See
@@ -79,4 +79,3 @@ Advanced Global Options:
           
           [default: true]
           [possible values: true, false]
-

crates/noseyparker-cli/tests/help/snapshots/test_noseyparker__help__help_report-2.snap
@@ -91,7 +91,7 @@ Advanced Global Options:
           [default: 16384]
 
       --sqlite-cache-size <SIZE>
-          Set the cache size for sqlite connections to SIZE
+          Set the cache size for SQLite connections to SIZE
           
           This has the effect of setting SQLite's `pragma cache_size=SIZE`. The default value is set
           to use a maximum of 1GiB for database cache. See

crates/noseyparker-cli/tests/help/snapshots/test_noseyparker__help__help_rules-2.snap
@@ -56,7 +56,7 @@ Advanced Global Options:
           [default: 16384]
 
       --sqlite-cache-size <SIZE>
-          Set the cache size for sqlite connections to SIZE
+          Set the cache size for SQLite connections to SIZE
           
           This has the effect of setting SQLite's `pragma cache_size=SIZE`. The default value is set
           to use a maximum of 1GiB for database cache. See
@@ -71,4 +71,3 @@ Advanced Global Options:
           
           [default: true]
           [possible values: true, false]
-

commit f464c0834600f62a4b176c731016dfc9e94eafe2

Fix startup assertion failure on <4GiB RAM machines (#202)
The v0.18.0 release crashes upon startup when running in an environment with less that 4GiB of RAM. The crash is an assertion failure from the `std::cmp::Ord::clamp` function when it is called with invalid arguments (i.e., `min > max`).

This crashing behavior was introduced in #174.

crates/noseyparker-cli/src/args.rs
@@ -103,7 +103,8 @@ fn default_scan_jobs() -> usize {
     match (std::thread::available_parallelism(), *RAM_GB) {
         (Ok(v), Some(ram_gb)) => {
             let n: usize = v.into();
-            n.clamp(1, (ram_gb / 4.0).floor() as usize)
+            let max_n = (ram_gb / 4.0).ceil().max(1.0) as usize;
+            n.clamp(1, max_n)
         }
         (Ok(v), None) => v.into(),
         (Err(_e), _) => 1,